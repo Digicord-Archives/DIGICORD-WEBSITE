40adecdd596575061d77914a6eb3ce583db54993
Elameen123 <69402639+Elameen123@users.noreply.github.com>
2022-06-22 22:37:25 +0100

    Section 4 of Landing Page Complete

--- digicord/src/pages/Landing-Page/Landing/Landing.js
+++ digicord/src/pages/Landing-Page/Landing/Landing.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import Landing_section1 from '../Section/Landing_section1/Landing_section1'
-import Landing_section2 from '../Section/Landing_section2/Landing_section2'
-import Landing_section3 from '../Section/Landing_section3/Landing_section3'
-import Landing_section4 from '../Section/Landing_section4/Landing_section4'
-import Landing_section5 from '../Section/Landing_section5/Landing_section5'
-import { Container } from 'react-bootstrap'
+import React from 'react';
+import Landingsection1 from '../Section/Landing_section1/Landingsection1';
+import Landingsection2 from '../Section/Landing_section2/Landingsection2';
+import Landingsection3 from '../Section/Landing_section3/Landingsection3';
+import Landingsection4 from '../Section/Landing_section4/Landingsection4';
+import Landingsection5 from '../Section/Landing_section5/Landingsection5';
+import { Container } from 'react-bootstrap';
+import './Landing.css';
 
 const Landing = () => {
   return (
    <Container>
-      <Landing_section1/>
-      <Landing_section2/>
-      <Landing_section3/>
-      <Landing_section4/>
-      <Landing_section5/>
+      <Landingsection1/>
+      <Landingsection2/>
+      <Landingsection3/>
+      <Landingsection4/>
+      <Landingsection5/>
    </Container>
   )
 }
--- digicord/src/pages/Landing-Page/Section/Landing_section2/Landingsection2.js
+++ digicord/src/pages/Landing-Page/Section/Landing_section2/Landingsection2.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Landing_section2 = () => {
+const Landingsection2 = () => {
   return (
     <div>Landing_section2</div>
   )
 }
 
-export default Landing_section2
+export default Landingsection2
