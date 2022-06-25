23f3f8233a02ce68096e6d1b202a88d608c92716
Elameen123 <69402639+Elameen123@users.noreply.github.com>
2022-06-25 15:06:57 +0100

    Pages arranged

--- digicord/src/pages/Landing-Page/Landing/Landing.js
+++ digicord/src/pages/Landing-Page/Landing/Landing.js
@@ -3,7 +3,7 @@ import Landingsection1 from '../Section/Landing_section1/Landingsection1';
 import Landingsection2 from '../Section/Landing_section2/Landingsection2';
 import Landingsection3 from '../Section/Landing_section3/Landingsection3';
 import Landingsection4 from '../Section/Landing_section4/Landingsection4';
-import Landingsection5 from '../Section/Landing_section5/Landingsection5';
+// import Landingsection5 from '../Section/Landing_section5/Landingsection5';
 import { Container } from 'react-bootstrap';
 import './Landing.css';
 
@@ -13,8 +13,8 @@ const Landing = () => {
       <Landingsection1/>
       <Landingsection2/>
       <Landingsection3/>
+      {/* <Landingsection5/> */}
       <Landingsection4/>
-      <Landingsection5/>
    </Container>
   )
 }
--- digicord/src/pages/Landing-Page/Section/Landing_section5/Landingsection5.js
+++ digicord/src/pages/Landing-Page/Section/Landing_section5/Landingsection5.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import './Landingsection5.css';
+// {/* <meta name="viewport"
+// content="width=device-width,
+// initial scale= 1"/>  */}
 
-const Landingsection5 = () => {
-  return (
-    <div>Landing_section5</div>
-  )
-}
+const Landingsection5= () => {
 
-export default Landingsection5
+	return (
+		
+		// <p>Subscribe to our Newsletter</p>
+    	<form>
+		<h1>
+			Subscribe To Our Newsletter
+		</h1>
+	   <div className='wrapper'>
+	    <div className='Newsletter'>
+      <input type="text" className="input" placeholder="Enter your Email Address">
+		</input>
+		<button type="submit" className='submit'>Subscribe</button>
+        </div>
+    </div>
+		</form>
+  
+    )
+     }
+
+export default Landingsection5;
